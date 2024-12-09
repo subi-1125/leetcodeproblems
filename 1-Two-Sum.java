1ab27dbbacfb1105ddbd12dd1a9e6ecb0bb03a70
subi-1125 <717822f159@kce.ac.in>
2024-12-10 00:31:47 +0530

    [1] [Time Beats: 98.91%] [Memory Beats: 53.55%] - LeetPush

---
+++ 1-Two-Sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) { 
+        Map<Integer, Integer> numMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+           
+            int complement = target - nums[i];  
+
+            if (numMap.containsKey(complement)) {
+                return new int[] {numMap.get(complement), i};
+            }
+            
+            numMap.put(nums[i], i);
+        }
+        
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    
+}
