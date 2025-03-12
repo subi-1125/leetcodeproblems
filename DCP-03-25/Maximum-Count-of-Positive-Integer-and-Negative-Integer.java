d77d9c46a9985c6438bd89470c04696fc4daac57
subi-1125 <717822f159@kce.ac.in>
2025-03-12 14:04:22 +0530

    [2529] [Time Beats: 100.00%] [Memory Beats: 48.41%] - LeetPush

---
+++ DCP-03-25/Maximum-Count-of-Positive-Integer-and-Negative-Integer.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maximumCount(int[] nums) {
+        int a=0;
+        int b=0;
+        for (int i=0;i<nums.length;i++){
+            if (nums[i]>0){
+                a++;
+            }
+            else if (nums[i]<0){
+                b++;
+            }
+        }
+        if (a>b){
+            return a;
+        }
+        return b;
+    }
+}
