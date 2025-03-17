ad632632f61776e6f044b137f9577c2c6611eb42
subi-1125 <717822f159@kce.ac.in>
2025-03-17 19:54:19 +0530

    [2206] [Time Beats: 22.48%] [Memory Beats: 85.16%] - LeetPush

---
+++ DCP-03-25/Divide-Array-Into-Equal-Pairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean divideArray(int[] nums) {
+        HashMap<Integer,Integer>mp = new HashMap<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
+        }
+        for(Map.Entry<Integer,Integer>entry:mp.entrySet())
+        {
+            if(entry.getValue()%2!=0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
